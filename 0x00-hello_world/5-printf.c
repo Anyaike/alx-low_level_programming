c55a0a57e83b924bac2578d438f7000a137ed95c
Anyaike <jochebedimcservices@gmail.com>
2022-10-20 02:52:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
- */
+* Main -  Entry point for the program
+*
+* Description: the function prints a string using the printf
+* function
+*
+* Return: 0
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n"
-;
-return (0);}
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
